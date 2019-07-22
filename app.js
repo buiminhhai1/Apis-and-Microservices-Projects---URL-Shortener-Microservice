526bb1b6ed50b1ea82f6ae1b93d96a7b839e41dd
buiminhhai1 <ngovietduc20088@gmail.com>
2019-07-22 09:54:02 +0700

    complete project

---
+++ app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var dns = require('dns');
+
+var db = require('./db');
+var validate = require('./validate');
+
+var cors = require('cors');
+
+var app = express();
+
+// Basic Configuration 
+var port = process.env.PORT || 3000;
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+  
+app.post("/api/shorturl/new",
+  async (req, res, next) => {
+    try {
+      await validate.validateURL(req.body.url);
+      next();
+    } catch (err) {
+      res.json({ error: 'invalid URL' });
+    }
+  },
+  async (req, res) => {
+    try {
+      const data = await db.findOrCreateURL(req.body.url);
+      res.json({ original_url: data.original_url, short_url: data.short_url });
+    } catch (error) {
+      res.json({ error });
+    }
+  }
+);
+
+app.get("/api/shorturl/all", async (req, res, next) => {
+  try {
+    const urls = await db.showAll();
+    res.json({ urls });
+  } catch (error) {
+    res.json({ error });
+  }
+})
+
+app.get("/api/shorturl/:shorturl", async (req, res, next) => {
+  const url = parseInt(req.params.shorturl);
+  if (isNaN(url)) res.json({ error: 'Wrong shorturl format' });
+  try {
+    const data = await db.shortUrlLookup(url);
+    res.redirect(data.original_url);
+  } catch (error) {
+    res.json({ error });
+  }
+});
+
+
+app.listen(port, function () {
+  console.log('Node.js listening ' + port);
+});
